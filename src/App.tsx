de1a251f5a0ba7f1368ccf67726ec4e8699e5376
Robert Kirsz <robert.kirsz@movingimage.com>
2021-05-31 17:22:20 +0200

    Add DarkModeSwitch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { useStore } from 'store'
+import DarkModeSwitch from 'DarkModeSwitch'
 import logo from 'logo.svg'
 
 export default function App() {
+  const { settings } = useStore()
+
   return (
-    <main>
+    <main className={settings.darkMode ? 'dark' : ''}>
       <img src={logo} alt="Logo" />
+      <DarkModeSwitch />
     </main>
   )
 }
